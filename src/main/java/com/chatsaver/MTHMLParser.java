53a7ee3085d6a68ab8f20de87b73e83aacaab8fc
Megan <megante.kerstetter@gmail.com>
2025-02-25 17:36:30 -0500

    Dependency Baking

---
+++ src/main/java/com/chatsaver/MTHMLParser.java
@@ -0,0 +1,27 @@
+package com.chatsaver;
+
+//importations
+import java.io, *;
+import java.nio.charset.StandardCharsets;
+//file handler
+import java.nio.file.Files;
+import java.nio.file.Paths;
+//Utilities
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+//Jsoup
+import org.json.Jsoup;
+import org.json.nodes.Document;
+import org.json.nodes.Element;
+import org.json.nodes.select.Elements;
+ 
+public class MTHMLParser {
+
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
